27b6ac784d06ba8370c69944c464b61e1aa11e38
sreekardev123 <sreesaikaranam27@gmail.com>
2025-01-06 14:30:19 +0530

    netflix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
       <Route path="/" element={<HomeScreen/>}/>
       <Route path="about-us" element={<AboutUs/>}/>
       <Route path="*" element={<NotFound/>}/>
-
     </Routes>
   );
 }
